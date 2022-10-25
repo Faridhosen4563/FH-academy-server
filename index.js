eb74f938c2f1f4e588e2ce09c1332acf20a29511
Faridhosen4563 <faridhosen790@gmail.com>
2022-10-26 01:24:30 +0600

    create course route and load data

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const app = express();
 
 const Port = process.env.Port || 5000;
 
+const course = require("./data/fakeData.json");
+
 app.get("/", (req, res) => {
   res.send("server is running");
+});
+
+app.get("/courses", (req, res) => {
+  res.send(course);
 });
 
 app.listen(Port, () => {
